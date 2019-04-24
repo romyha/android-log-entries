0cda271d63e74daecfa182e2e6845882bc75a078
Romy Haacke <romy@haacke.tv>
2019-04-24 15:06:59 +0200

    initial commit

---
+++ app/src/main/java/com/example/myapplication/AddLogActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class AddLogActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_log);
+
+
+    }
+
+    /** Called when the user taps the Send button */
+    public void addLog(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, ViewLogsActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openAddLogActivity(View view) {
+        Intent intent = new Intent(this, AddLogActivity.class);
+        startActivity(intent);
+    }
+}
