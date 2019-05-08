2cdd0d18e7045ec93882233be3a2b7f7bcc2ab00
Romy Haacke <romy@haacke.tv>
2019-05-08 14:04:34 +0200

    add persistent storage for log

--- app/src/main/java/com/example/myapplication/AddLogActivity.java
+++ app/src/main/java/com/example/myapplication/AddLogActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,8 +26,17 @@ public class AddLogActivity extends AppCompatActivity {
         // Do something in response to button
         Intent intent = new Intent(this, ViewLogsActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
+        EditText idEdit = (EditText) findViewById(R.id.editText3);
         String message = editText.getText().toString();
+        String id = idEdit.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("LOGS", MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(id, message);
+        editor.commit();
+
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,32 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView welcome = findViewById(R.id.editText2);
+        TextView lastLog = findViewById(R.id.textView4);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("LOGS", MODE_PRIVATE);
+        String lastSavedLog = (sharedPreferences.getString("1", ""));
+
+        lastLog.setText("Last " + lastSavedLog);
+        welcome.setSingleLine(false);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = format.format( new Date()   );
+        welcome.setText("Hi Hatem, nice to meet you!\nToday is the " + dateString + "\nThis was your last log:");
     }
 
     public void openAddLogActivity(View view) {
