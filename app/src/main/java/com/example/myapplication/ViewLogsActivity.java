91eec2dbf535b961c3995883fd54b809f7cdf175
Romy Haacke <romy@haacke.tv>
2019-05-08 15:24:34 +0200

    add nav_graph and nav_host_fragment

--- app/src/main/java/com/example/myapplication/ViewLogsActivity.java
+++ app/src/main/java/com/example/myapplication/ViewLogsActivity.java
@@ -1,11 +1,17 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import java.util.Collection;
+import java.util.Map;
+
 public class ViewLogsActivity extends AppCompatActivity {
+    public Collection<?> entries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +21,16 @@ public class ViewLogsActivity extends AppCompatActivity {
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
         String message = intent.getStringExtra(AddLogActivity.EXTRA_MESSAGE);
+        SharedPreferences sharedPreferences = getSharedPreferences("LOGS", MODE_PRIVATE);
+        entries = sharedPreferences.getAll().values();
+        String logs = "";
+        for (Object log : entries) {
+            logs += log.toString() + "\n";
+            //Log.d("VALUE", s.toString());
+        }
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView2);
-        textView.setText(message);
+        textView.setText(logs);
     }
 }
