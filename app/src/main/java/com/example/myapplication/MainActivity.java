e9483a964ce80f983d41ba9aeec7b10604b76c99
Romy Haacke <romy@haacke.tv>
2019-05-29 15:56:01 +0200

    add bluetooth functionality for sending data from preferences

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -33,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         if (android.os.Build.VERSION.SDK_INT >= 23) {
             requestPermissions(new String[]{
                             Manifest.permission.INTERNET,
+                            Manifest.permission.BLUETOOTH,
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
                             Manifest.permission.READ_EXTERNAL_STORAGE,
                             Manifest.permission.WRITE_EXTERNAL_STORAGE,
                             Manifest.permission.ACCESS_NETWORK_STATE},
@@ -50,35 +51,6 @@ public class MainActivity extends AppCompatActivity {
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         String dateString = format.format( new Date()   );
         welcome.setText("Hi Hatem, nice to meet you!\nToday is the " + dateString + "\nThis was your last log:");
-
-        // TCP
-        serverTransmitButton = (Button) findViewById(R.id.button_TCP_server);
-        serverTransmitButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Log.i("Start Server Clicked", "yipee");
-
-                new openServerTask().execute();
-                Log.i("socketConnection", "JAY, we got a connection!");
-
-                //open a file manager to let user choose desired file.
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("file/*");
-                startActivityForResult(intent, PICKFILE_REQUEST_CODE);
-            }
-        });
-
-        clientReceiveButton = (Button) findViewById(R.id.button_TCP_client);
-        clientReceiveButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                Log.i("Read Button Clicked", "yipee");
-                /**
-                 * Create a client socket with the host,
-                 * port, and timeout information.
-                 */
-                new ConnectTask().execute();
-            }
-        });
     }
 
     public void openAddLogActivity(View view) {
@@ -88,6 +60,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void openViewLogsActivity(View view) {
         Intent intent = new Intent(this, ViewLogsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openBTActivity(View view) {
+        Intent intent = new Intent(this, BTActivity.class);
         startActivity(intent);
     }
 }
