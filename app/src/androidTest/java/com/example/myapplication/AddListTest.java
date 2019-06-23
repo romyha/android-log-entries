4267ff2676dbc6530f517044833fa3ef13d8d568
Hatem Fayed <hatem.fayed@student.htw-berlin.de>
2019-06-23 20:13:37 +0200

    Adding a new item test

--- app/src/androidTest/java/com/example/myapplication/AddListTest.java
+++ app/src/androidTest/java/com/example/myapplication/AddListTest.java
@@ -9,19 +9,19 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static org.junit.Assert.*;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 public class AddListTest {
 
     @Rule
     public ActivityTestRule<AddList> mActivityTestRule = new ActivityTestRule<AddList>(AddList.class);
 
-    private String nName = "List";
+    private String listName = "TestList";
+    private String itemName = "TestItem";
 
     @Before
     public void setUp() throws Exception {
@@ -29,10 +29,10 @@ public class AddListTest {
 
     @Test
     public void testUserListNameInputScenario() {
-        //perform button click
+        //perform button click (New  List)
         Espresso.onView(withId(R.id.button3)).perform(click());
         //input text in the edit text
-        Espresso.onView(withId(R.id.listName)).perform(typeText(nName));
+        Espresso.onView(withId(R.id.listName)).perform(typeText(listName));
         //perform button click
         Espresso.onView(withId(R.id.button)).perform(click());
         //close soft keyboard
@@ -40,7 +40,27 @@ public class AddListTest {
         //go back
         Espresso.pressBack();
         //checking text in text view
-        Espresso.onView(withId(R.id.shoppingLists)).check(matches(withText(nName)));
+        Espresso.onView(withId(R.id.shoppingLists)).check(matches(withText(listName)));
+    }
+
+    @Test
+    public void testAddItemScenario(){
+        //perform button click (New  List)
+        Espresso.onView(withId(R.id.button3)).perform(click());
+        //input text in the edit text (List Name)
+        Espresso.onView(withId(R.id.listName)).perform(typeText(listName));
+        //perform button click (Save)
+        Espresso.onView(withId(R.id.button)).perform(click());
+        //input text in the edit text (List Item)
+        Espresso.onView(withId(R.id.item)).perform(typeText(itemName));
+        //perform button click (Add Item)
+        Espresso.onView(withId(R.id.button5)).perform(click());
+        //close soft keyboard
+        Espresso.closeSoftKeyboard();
+        //perform button click (Show List)
+        Espresso.onView(withId(R.id.button4)).perform(click());
+        //checking text in text view
+        Espresso.onView(withId(R.id.scrollView2)).check(matches(withText(itemName)));
     }
 
     @After
